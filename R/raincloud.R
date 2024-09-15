c18dea75bee859da1d6d3b867873e373d706c602
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2024-09-15 00:06:35 -0400

    added raincloud function

---
+++ R/raincloud.R
@@ -0,0 +1,42 @@
+#' raincloud
+#'
+#' ggplot2 raincloud plot template.
+#'
+#' @param data Default dataset to use for plot.
+#' @param mapping Default list of aesthetic mappings to use for plot.
+#' @param flip_coords Default orientation is vertical (FALSE),
+#' set to TRUE for horizontal.
+#'
+#' @param ... Additional parameters.
+#'
+#' @return Returns a raincloud ggplot.
+#'
+#' @examples
+#' # Example usage of the function
+#' plot <- raincloud(iris, aes(Species, Sepal.Width, color = Species))
+#'
+#' @import ggplot2
+#' @import ggforce
+#' @import ggdist
+#' @import gghalves
+#'
+#' @export
+raincloud <- function(data, mapping, flip_coords = FALSE, ...) {
+
+  plot <- ggplot2::ggplot(data, mapping) +
+    ggdist::stat_halfeye(
+      adjust = .5,
+      width = .7,
+      .width = 0,
+      justification = -.2,
+      point_colour = NA
+    ) +
+    ggplot2::geom_boxplot(width = .2, outlier.shape = NA) +
+    ggplot2::geom_jitter(width = .05, alpha = .3)
+
+  if (flip_coords) {
+    plot <- plot + ggplot2::coord_flip(xlim = c(1, NA), clip = "off")
+  }
+
+  return(plot)
+}
